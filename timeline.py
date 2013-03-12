67dbf480bb4e1d9664575520dfc0762362c0bc5f
SimmiAnand <simmi.anand@openlabs.co.in>
2013-03-12 18:48:08 +0530

    Modified Billing Type field
    *Added context for the section field
    *which passes selected value to the inherited field as default

--- timeline.py
+++ timeline.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-    
+    Timeline
 
     :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.transaction import Transaction
+from trytond.pyson import Eval, Not
+
 
 __all__ = ['Timeline']
 __metaclass__ = PoolMeta
@@ -14,8 +17,18 @@ __metaclass__ = PoolMeta
 
 class Timeline:
     __name__ = 'timesheet.line'
-
     billing_type = fields.Selection([
         ('billable', 'Billable'),
         ('non billable', 'Non Billable'),
-    ], 'Billing Type', required=True, select=True)
+    ], 'Billing Type', required=True, select=True,
+    states={
+        'readonly': Not(Eval('billing_type')== 'billable')
+    })
+
+    @staticmethod
+    def default_billing_type():
+        return Transaction().context.get('billable')
+
+class Task:
+    __name__= 'project.work'
+
