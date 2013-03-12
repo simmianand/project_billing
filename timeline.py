e9bcae00bdd4740bb9db9f644481f900bfb4efb0
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2013-03-12 10:57:28 +0530

    Converted to blank

--- timeline.py
+++ timeline.py
@@ -5,15 +5,3 @@
     :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
-from trytond.model import ModelView, ModelSQL, fields
-from trytond.pool import PoolMeta
-
-
-__all__ = ['Timeline']
-__metaclass__ = PoolMeta
-
-
-class Timeline(ModelSQL, ModelView):
-    'Timeline'
-    __name__ = 'project.timeline'
-    dummy = fields.Numeric('dummy')
